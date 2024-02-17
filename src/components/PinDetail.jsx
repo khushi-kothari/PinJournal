833941b0c17d9c93fc8c89334d2fad33aeabef77
Khushi Kothari <62203565+khushi-kothari@users.noreply.github.com>
2024-02-17 23:54:09 +0530

    Edit PinDetail page

--- src/components/PinDetail.jsx
+++ src/components/PinDetail.jsx
@@ -7,6 +7,8 @@ import { v4 as uuidv4 } from 'uuid';
 import MasonryLayout from './MasonryLayout';
 import { pinDetailMorePinQuery, pinDetailQuery } from '../utils/data';
 import Spinner from './Spinner';
+import { collection, doc, getDoc, getDocs, query, where } from 'firebase/firestore';
+import db from '../firestore';
 
 
 const PinDetail = ({ user }) => {
@@ -41,22 +43,80 @@ const PinDetail = ({ user }) => {
   }
 
 
-  const fetchPinDetails = () => {
-    let query = pinDetailQuery(pinId);
+  const fetchPinDetails = async () => {
+    // let query = pinDetailQuery(pinId);
+    const docRef = doc(db, "Pins", pinId);
+    const pinSnap = (await getDoc(docRef)).data();
+    let arr = [];
+    arr.push(pinSnap);
+    const updatedData = await Promise.all(arr.map(async (d) => {
+      const createdDocRef = doc(db, "Users", d.CreatedBy.id)
+      const createdArr = (await getDoc(createdDocRef)).data();
+      const savedArr = await Promise.all(d.SavedBy.map(async (s) => {
+        const docRef = doc(db, "Users", s.id);
+        let data = (await getDoc(docRef)).data();
+        return data;
+      }));
+      const commentedArr = await Promise.all(d.Comments.map(async (c) => {
+        const docRef = doc(db, "Users", c.CommentedBy.id)
+        let data = (await getDoc(docRef)).data();
+        return data;
+      }));
 
-    if (query) {
-      // client.fetch(query)
-      //   .then((data) => {
-      //     setPinDetail(data[0]);
+      for (let i = 0; i < commentedArr.length; i++) {
+        d.Comments[i].CommentedBy = commentedArr[i];
+      }
 
-      //     if (data[0]) {
-      //       query = pinDetailMorePinQuery(data[0]);
-      //       client.fetch(query)
-      //         .then((res) => setPins(res));
-      //     }
-      //   })
+      return { ...d, CreatedBy: createdArr, SavedBy: savedArr }
+    }));
 
+    setPinDetail(updatedData[0]);
+    console.log('Doc Snap', updatedData[0]);
+
+    if (pinSnap) {
+      const collectionRef = collection(db, "Pins");
+      const q = query(collectionRef, where("Pin.Category", "==", pinSnap.Pin.Category));
+      const snapshot = await getDocs(q);
+      // console.log(snapshot.docs); //Array of docs but no useful data
+      const results = snapshot.docs.map(doc => ({ ...doc.data() }));
+      const updatedData = await Promise.all(results.map(async (d) => {
+        const createdDocRef = doc(db, "Users", d.CreatedBy.id)
+        const createdArr = (await getDoc(createdDocRef)).data();
+        const savedArr = await Promise.all(d.SavedBy.map(async (s) => {
+          const docRef = doc(db, "Users", s.id);
+          let data = (await getDoc(docRef)).data();
+          return data;
+        }));
+        const commentedArr = await Promise.all(d.Comments.map(async (c) => {
+          const docRef = doc(db, "Users", c.CommentedBy.id)
+          let data = (await getDoc(docRef)).data();
+          return data;
+        }));
+
+        for (let i = 0; i < commentedArr.length; i++) {
+          d.Comments[i].CommentedBy = commentedArr[i];
+        }
+
+        return { ...d, CreatedBy: createdArr, SavedBy: savedArr }
+      }));
+      console.log(updatedData);
+      setPins(updatedData)
     }
+
+    // if (query) {
+
+    //   // client.fetch(query)
+    //   //   .then((data) => {
+    //   //     setPinDetail(data[0]);
+
+    //   //     if (data[0]) {
+    //   //       query = pinDetailMorePinQuery(data[0]);
+    //   //       client.fetch(query)
+    //   //         .then((res) => setPins(res));
+    //   //     }
+    //   //   })
+
+    // }
   }
 
   useEffect(() => {
@@ -71,14 +131,14 @@ const PinDetail = ({ user }) => {
         style={{ maxWidth: '1500px', borderRadius: '32px' }}>
         <div className='flex justify-center items-center md:items-start flex-initial'>
           <img
-            src={pinDetail?.image && urlFor(pinDetail.image).url()}
+            src={pinDetail?.Pin.image}
             className='rounded-t-3xl rounded-b-lg'
             alt="user-post" />
         </div>
         <div className='w-full p-5 flex-1 xl:min-w-620'>
           <div className='flex items-center justify-between'>
             <div className='flex gap-2 items-center'>
-              <a href={`${pinDetail.image.asset.url}?dl=`}
+              <a href={`${pinDetail?.Pin.image}?dl=`}
                 onClick={(e) => {
                   e.stopPropagation();
                 }}
@@ -86,40 +146,41 @@ const PinDetail = ({ user }) => {
               ><MdDownloadForOffline />
               </a>
             </div>
-            <a href={pinDetail.destination} target="_blank" rel="noreferrer" className=' w-36 truncate overflow-hidden' >
-              {pinDetail.destination}
+            <a href={pinDetail.Pin.url} target="_blank" rel="noreferrer" className=' w-36 truncate overflow-hidden' >
+              {pinDetail.Pin.url}
             </a>
           </div>
           <div>
-            <h1 className='text-4xl font-bold break-words mt-3'>{pinDetail.title}</h1>
-            <p className='mt-3'>{pinDetail.about}</p>
+            <h1 className='text-4xl font-bold break-words mt-3'>{pinDetail.Pin.Title}</h1>
+            <p className='mt-3'>{pinDetail.Pin.About}</p>
+            <p className='mt-3'>{pinDetail.Pin.Description}</p>
           </div>
-          <Link to={`/user-profile/${pinDetail.postedBy?._id}`} className="flex gap-2 mt-5 items-center bg-white rounded-lg">
+          <Link to={`/user-profile/${pinDetail.CreatedBy?.id}`} className="flex gap-2 mt-5 items-center bg-white rounded-lg">
             <img
               className="w-8 h-8 rounded-full object-cover"
-              src={pinDetail.postedBy?.image}
+              src={pinDetail.CreatedBy?.image}
               alt="user-profile"
             />
-            <p className=" font-medium capitalize">{pinDetail.postedBy?.userName}</p>
+            <p className=" font-medium capitalize">{pinDetail.CreatedBy?.userName}</p>
           </Link>
           <h2 className='mt-5 text-2xl'>Comments</h2>
-          <div className='max-h-370 overflow-y-auto'>{pinDetail?.comments?.map((comment, i) => (
+          <div className='max-h-370 overflow-y-auto'>{pinDetail?.Comments?.map((comment, i) => (
             <div className='flex gap-2 mt-5 items-center bg-white rounded-lg' key={i}>
-              <img src={comment.postedBy.image}
+              <img src={comment.CommentedBy.image}
                 alt="user-profile"
                 className='w-10 h-10 rounded-full cursor-pointer' />
               <div className='flex flex-col'>
-                <p className='font-bold'>{comment.postedBy.userName}</p>
-                <p>{comment.comment}</p>
+                <p className='font-bold'>{comment.CommentedBy.name}</p>
+                <p>{comment.Comment}</p>
               </div>
             </div>
           ))}
           </div>
           <div className='flex flex-wrap mt-6 gap-3'>
-            <Link to={`/user-profile/${pinDetail.postedBy?._id}`}>
+            <Link to={`/user-profile/${pinDetail.CreatedBy?.id}`}>
               <img
                 className="w-8 h-8 rounded-full cursor-pointer"
-                src={pinDetail.postedBy?.image}
+                src={pinDetail.CreatedBy?.image}
                 alt="user-profile"
               />
             </Link>
